609da1cbbd0c13dd9d8425a0cabbc447820aed0a
Asad Hadi <78380751+Chamooda@users.noreply.github.com>
2023-12-27 22:24:10 +0530

    Update Main.py

--- IITB_Final/src/Main.py
+++ IITB_Final/src/Main.py
@@ -1,11 +1,15 @@
+#Importing necessary libraries
 from uagents.setup import fund_agent_if_low
 from uagents import Agent, Bureau, Context, Model
 
+#Importing agents from python modules
 from Agents.Alice import alice
 from Agents.Converter import Converter
 
+#Adding agents in an object of class Bureau
 bureau = Bureau()
 bureau.add(alice)
 bureau.add(Converter)
 
+#Adding agents in an object of class Bureau
 bureau.run()
